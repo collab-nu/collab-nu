e8836f70ed2db30872dda151e74dbfa5e9f02a73
akeller98 <aaronkeller2021@u.northwestern.edu>
2019-05-15 19:48:16 -0500

    Added redirect and JS

---
+++ js/home_default.js
@@ -0,0 +1,12 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function redirect() {
+  var login = document.getElementById("login");
+  location.href = "/home_user.html";
+}
