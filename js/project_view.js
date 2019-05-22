128ae07fb2011703e7c0988278f2cfcde2384862
grfirmo <rdgdem@gmail.com>
2019-05-22 16:07:35 -0500

    Built the pop up for application + distinct pages for view vs own

---
+++ js/project_view.js
@@ -0,0 +1,14 @@
+function showOverlay(id) {
+  console.log(id + 'apply')
+  var overlay = document.getElementById(id + '-apply');
+  overlay.style.display = "block";
+  console.log(overlay);
+}
+
+function hideOverlay(id) {
+  var overlay = document.getElementById(id + '-apply');
+  overlay.style.display = "none";
+  var button = document.getElementById(id + '-button');
+  button.innerHTML = "Applied";
+  button.disabled = "true";
+}
