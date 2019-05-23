a17cf80dbbbcd057cfe475c6d6ab34954c6c558c
Karen Bao <karenbao2021@u.northwestern.edu>
2019-05-22 21:30:48 -0500

    Made filter popup

--- js/home_default.js
+++ js/home_default.js
@@ -6,6 +6,8 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
+
+
 function redirect() {
   var login = document.getElementById("login");
   location.href = "/home_user.html";
