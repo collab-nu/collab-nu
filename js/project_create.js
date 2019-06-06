4c246afce6928a35f39aa49d16f19b9e1b0b02be
akeller98 <aaronkeller2021@u.northwestern.edu>
2019-06-05 20:54:24 -0500

    more buttons

--- js/project_create.js
+++ js/project_create.js
@@ -5,7 +5,7 @@ function openRole() {
   var positions = document.getElementById('positions');
   var buttonPos = document.getElementById('addPosition');
   var newForm = document.createElement('div');
-  newForm.innerHTML = "<input class='newPos' type='text' placeholder='Position name...' /> <input class=doneButton id=but"+ i + " type='button' value='Done' onClick='closeRole(this.id)'>";
+  newForm.innerHTML = "<input class='newPos' type='text' placeholder='Position name...' /> <input class='btn doneButton' id=but"+ i + " type='button' value='Done' onClick='closeRole(this.id)'>";
   newForm.classList.add('posForm');
   positions.appendChild(newForm);
   buttonPos.style.display = "none";
